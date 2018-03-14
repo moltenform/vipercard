f90b66bfe09eeb36485b9552a9e3fcbb1484b9af
Ben Fisher <downpoured@gmail.com>
2018-03-13 19:36:23 -0700

    Like the logo, use version 0.2

--- src/appsettings.ts
+++ src/appsettings.ts
@@ -2,7 +2,7 @@
 
 // xx (major version) yy (minor version)
 // zz (20=dev 40=alpha 60=beta 80=final) rr build number
-export const vpcversion = "10002000";
+export const vpcversion = "00002000";
 export const isRelease = false;
 export const projectBuildStamp1 = 67;
 export const projectBuildStamp2 = 247;
